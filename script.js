286e0c94e7d3c961babcbf0619fb09e2a3927d5e
Eric Junior <eric.faria2003@gmail.com>
2024-04-24 21:47:30 -0300

    Criado Arquivo CSS e JavaScript, adicionado Pasta Imagens

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.expandable');
+    const overlay = document.getElementById('overlay');
+  
+    images.forEach(image => {
+      image.addEventListener('click', function () {
+        const expandedImage = document.createElement('img');
+        expandedImage.src = this.src;
+        expandedImage.alt = this.alt;
+        expandedImage.classList.add('expanded-image');
+  
+        overlay.innerHTML = ''; // Limpa qualquer conteúdo anterior
+        overlay.appendChild(expandedImage);
+        overlay.style.display = 'flex'; // Alterado para 'flex' para centralizar a imagem na tela
+  
+        // Adicionado um evento de clique para fechar a imagem expandida
+        overlay.addEventListener('click', function () {
+          overlay.style.display = 'none';
+        });
+      });
+    });
+  });
+  
