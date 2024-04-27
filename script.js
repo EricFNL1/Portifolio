7a75d218648d201bfe1bf64f28fdd5c91dbc4b03
Eric Junior <eric.faria2003@gmail.com>
2024-04-26 23:10:10 -0300

    Alterado CSS Index e JS, colocado carrosel e efeitod e pulso

--- script.js
+++ script.js
@@ -20,4 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
   
