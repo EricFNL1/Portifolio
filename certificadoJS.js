2701d927aa755c77d276e63bc6efc133a311ed3c
Eric Notebook <eric.faria2003@gmail.com>
2024-08-23 20:31:21 -0300

    Criado página para certificados e tecnologias usadas/ criado botoẽs para navegação entre páginas

---
+++ certificadoJS.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll('.animate-card');
+
+    // Adiciona a classe 'fade-in' aos cards para que a animação seja aplicada ao carregar a página
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.add('fade-in');
+        }, index * 150); // Adiciona um pequeno atraso entre cada card para um efeito escalonado
+    });
+});
