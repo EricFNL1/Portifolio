65f713c2bf50cf161053049471dd253d816b6284
Eric Notebook <eric.faria2003@gmail.com>
2024-08-23 21:30:13 -0300

    Redirecionado detalhes de páginas, versão beta começada

--- certificadoJS.js
+++ certificadoJS.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Adiciona a classe 'fade-in' aos cards para que a animação seja aplicada ao carregar a página
     cards.forEach((card, index) => {
         setTimeout(() => {
-            card.classList.add('fade-in');
+            card.classList.add('fade-in-active');
         }, index * 150); // Adiciona um pequeno atraso entre cada card para um efeito escalonado
     });
 });
